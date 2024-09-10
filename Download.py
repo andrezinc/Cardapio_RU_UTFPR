5889117d12656db32b21e46220c6ca5f661540b7
Andre Castilhano <71910133+andrezinc@users.noreply.github.com>
2024-09-09 22:30:53 -0300

    Update Download.py

--- Download.py
+++ Download.py
@@ -7,7 +7,7 @@ def download():
     gdown.download_folder(URL_RU,output='Cardapio') #sera baixado o cardapio 
 def totext():
     if(os.system("pdftotext -layout -fixed 13 Cardapio/"+str(day_)+"*.pdf")>0): #sera executado no sistema pega o numero do dia atual e transforma para txt
-     	os.system("pdftotext -layout -fixed 13 Cardapio/"+'0'+str(day_)+"*.pdf") #caso der erro ele vai colocar um 0 na frente pois os cardapios vem com 0(dia atual)
+     	os.system("pdftotext -layout -fixed 13 Cardapio/"+'*'+str(day_)+"*") #caso der erro ele vai colocar um 0 na frente pois os cardapios vem com 0(dia atual)
 def clean():
     os.system("cd .. & rm -r Cardapio") #limpa o arquivo anterior
 def main():
